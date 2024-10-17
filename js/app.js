40def6e9d56ed78d3af022b4809dd7a25dc7329d
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-17 16:09:56 +0200

    ADD: email authorization exercise

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("ciao")
+// E-mail
+
+const emailAutorizzate = ["spongebob@gmail.com", "patrick@gmail.com", "squiddi@gmail.com"];
+const emailUtente = prompt("Inserisci la tua email:");
+
+if (emailAutorizzate.includes(emailUtente)) {
+    let nomeUtente = emailUtente.split("@")[0];
+    nomeUtente = nomeUtente.charAt(0).toUpperCase() + nomeUtente.slice(1).toLowerCase();
+    console.log(`Benvenuto ${nomeUtente}!`);
+} else {
+    console.log(`Mi dispiace, ${emailUtente} non è autorizzata`);
+}
