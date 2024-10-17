99d4c43f4d1137bbed65bf9ada705fb11685701c
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-17 16:15:59 +0200

    ADD: second method for authorized emails

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-// E-mail
-
+// // E-mail
 const emailAutorizzate = ["spongebob@gmail.com", "patrick@gmail.com", "squiddi@gmail.com"];
 const emailUtente = prompt("Inserisci la tua email:");
 
@@ -10,3 +9,20 @@ if (emailAutorizzate.includes(emailUtente)) {
 } else {
     console.log(`Mi dispiace, ${emailUtente} non è autorizzata`);
 }
+
+
+//Secondo metodo
+// let emailTrovata = false;
+
+// for (let i = 0; i < emailAutorizzate.length; i++) {
+//     if (emailUtente === emailAutorizzate[i]) {
+//         emailTrovata = true;
+//     }
+// }
+// if (emailTrovata === true) {
+//     let nomeUtente = emailUtente.split("@")[0];
+//     nomeUtente = nomeUtente.charAt(0).toUpperCase() + nomeUtente.slice(1).toLowerCase();
+//     console.log(`Benvenuto ${nomeUtente}!`);
+// } else {
+//     console.log(`Mi dispiace, ${emailUtente} non è autorizzata`);
+// }
